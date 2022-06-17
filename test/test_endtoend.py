eb27c6e125ba3960d20852602cd86c26f2fe264b
Antonio Flores Montoya <afloresmontoya@grammatech.com>
2022-06-17 14:07:27 -0400

    add alias to shorten test code

--- test/test_endtoend.py
+++ test/test_endtoend.py
@@ -31,6 +31,8 @@ from retypd.solver import SolverConfig
 
 
 VERBOSE_TESTS = False
+parse_var = SchemaParser.parse_variable
+parse_cs = SchemaParser.parse_constraint
 
 
 def compute_sketches(
@@ -48,7 +50,7 @@ def compute_sketches(
 
         proc_parsed_cs = ConstraintSet()
         for c in proc_cs:
-            proc_parsed_cs.add(SchemaParser.parse_constraint(c))
+            proc_parsed_cs.add(parse_cs(c))
         parsed_cs[DerivedTypeVariable(proc)] = proc_parsed_cs
 
     parsed_callgraph = {
@@ -67,24 +69,20 @@ class RecursiveSchemaTest(unittest.TestCase):
         """A test based on the running example from the paper (Figure 2 on p. 3) and the slides
         (slides 67-83, labeled as slides 13-15).
         """
-        F = SchemaParser.parse_variable("F")
-        close = SchemaParser.parse_variable("close")
+        F = parse_var("F")
+        close = parse_var("close")
 
         constraints = {F: ConstraintSet(), close: ConstraintSet()}
-        constraints[F].add(SchemaParser.parse_constraint("F.in_0 ⊑ δ"))
-        constraints[F].add(SchemaParser.parse_constraint("α ⊑ φ"))
-        constraints[F].add(SchemaParser.parse_constraint("δ ⊑ φ"))
-        constraints[F].add(SchemaParser.parse_constraint("φ.load.σ4@0 ⊑ α"))
-        constraints[F].add(SchemaParser.parse_constraint("φ.load.σ4@4 ⊑ α'"))
-        constraints[F].add(SchemaParser.parse_constraint("α' ⊑ close.in_0"))
-        constraints[F].add(SchemaParser.parse_constraint("close.out ⊑ F.out"))
+        constraints[F].add(parse_cs("F.in_0 ⊑ δ"))
+        constraints[F].add(parse_cs("α ⊑ φ"))
+        constraints[F].add(parse_cs("δ ⊑ φ"))
+        constraints[F].add(parse_cs("φ.load.σ4@0 ⊑ α"))
+        constraints[F].add(parse_cs("φ.load.σ4@4 ⊑ α'"))
+        constraints[F].add(parse_cs("α' ⊑ close.in_0"))
+        constraints[F].add(parse_cs("close.out ⊑ F.out"))
 
-        constraints[close].add(
-            SchemaParser.parse_constraint("close.in_0 ⊑ #FileDescriptor")
-        )
-        constraints[close].add(
-            SchemaParser.parse_constraint("#SuccessZ ⊑ close.out")
-        )
+        constraints[close].add(parse_cs("close.in_0 ⊑ #FileDescriptor"))
+        constraints[close].add(parse_cs("#SuccessZ ⊑ close.out"))
 
         program = Program(DummyLattice(), {}, constraints, {F: [close]})
         solver = Solver(program, verbose=VERBOSE_TESTS)
@@ -94,16 +92,12 @@ class RecursiveSchemaTest(unittest.TestCase):
         F_sketches = sketches[F]
         # Equivalent to "#SuccessZ ⊑ F.out"
         self.assertEqual(
-            F_sketches.lookup(
-                SchemaParser.parse_variable("F.out")
-            ).lower_bound,
-            SchemaParser.parse_variable("#SuccessZ"),
+            F_sketches.lookup(parse_var("F.out")).lower_bound,
+            parse_var("#SuccessZ"),
         )
         self.assertEqual(
-            F_sketches.lookup(
-                SchemaParser.parse_variable("F.in_0.load.σ4@4")
-            ).upper_bound,
-            SchemaParser.parse_variable("#FileDescriptor"),
+            F_sketches.lookup(parse_var("F.in_0.load.σ4@4")).upper_bound,
+            parse_var("#FileDescriptor"),
         )
 
     def test_recursive_through_procedures(self):
@@ -134,13 +128,9 @@ class RecursiveSchemaTest(unittest.TestCase):
         )
 
         g_sketch = sketches[DerivedTypeVariable("g")]
-        self.assertIsNotNone(
-            g_sketch.lookup(SchemaParser.parse_variable("g.in_0.load.σ4@4"))
-        )
+        self.assertIsNotNone(g_sketch.lookup(parse_var("g.in_0.load.σ4@4")))
         self.assertEqual(
-            g_sketch.lookup(
-                SchemaParser.parse_variable("g.in_0.load.σ4@4")
-            ).upper_bound,
+            g_sketch.lookup(parse_var("g.in_0.load.σ4@4")).upper_bound,
             DummyLattice._int,
         )
         gen = CTypeGenerator(sketches, lattice, CLatticeCTypes(), 4, 4)
@@ -189,38 +179,30 @@ class RecursiveSchemaTest(unittest.TestCase):
         )
 
         f_sketch = sketches[DerivedTypeVariable("f")]
-        self.assertIsNotNone(
-            f_sketch.lookup(SchemaParser.parse_variable("f.in_0.load.σ4@0"))
-        )
+        self.assertIsNotNone(f_sketch.lookup(parse_var("f.in_0.load.σ4@0")))
         # we should have this capability even if we don't know the type
         # of the field
-        self.assertIsNotNone(
-            f_sketch.lookup(SchemaParser.parse_variable("f.in_0.load.σ4@12"))
+        self.assertIsNotNone(f_sketch.lookup(parse_var("f.in_0.load.σ4@12")))
+        self.assertEqual(
+            f_sketch.lookup(parse_var("f.in_0.load.σ4@0")),
+            f_sketch.lookup(parse_var("f.in_0")),
         )
         self.assertEqual(
-            f_sketch.lookup(SchemaParser.parse_variable("f.in_0.load.σ4@0")),
-            f_sketch.lookup(SchemaParser.parse_variable("f.in_0")),
-        )
-        self.assertEqual(
-            f_sketch.lookup(SchemaParser.parse_variable("f.in_0.load.σ4@4")),
-            f_sketch.lookup(SchemaParser.parse_variable("f.in_0")),
+            f_sketch.lookup(parse_var("f.in_0.load.σ4@4")),
+            f_sketch.lookup(parse_var("f.in_0")),
         )
 
         # check that information is transferred correctly to "g"
         g_sketch = sketches[DerivedTypeVariable("g")]
-        self.assertIsNotNone(
-            g_sketch.lookup(SchemaParser.parse_variable("g.in_0.load.σ4@0"))
-        )
-        self.assertIsNotNone(
-            g_sketch.lookup(SchemaParser.parse_variable("g.in_0.load.σ4@12"))
+        self.assertIsNotNone(g_sketch.lookup(parse_var("g.in_0.load.σ4@0")))
+        self.assertIsNotNone(g_sketch.lookup(parse_var("g.in_0.load.σ4@12")))
+        self.assertEqual(
+            g_sketch.lookup(parse_var("g.in_0.load.σ4@0")),
+            g_sketch.lookup(parse_var("g.in_0")),
         )
         self.assertEqual(
-            g_sketch.lookup(SchemaParser.parse_variable("g.in_0.load.σ4@0")),
-            g_sketch.lookup(SchemaParser.parse_variable("g.in_0")),
-        )
-        self.assertEqual(
-            g_sketch.lookup(SchemaParser.parse_variable("g.in_0.load.σ4@4")),
-            g_sketch.lookup(SchemaParser.parse_variable("g.in_0")),
+            g_sketch.lookup(parse_var("g.in_0.load.σ4@4")),
+            g_sketch.lookup(parse_var("g.in_0")),
         )
         # check C types
         gen = CTypeGenerator(sketches, lattice, CLatticeCTypes(), 4, 4)
@@ -269,22 +251,18 @@ class RecursiveSchemaTest(unittest.TestCase):
         )
         # check that information is transferred correctly to "g"
         g_sketch = sketches[DerivedTypeVariable("g")]
-        self.assertIsNotNone(
-            g_sketch.lookup(SchemaParser.parse_variable("g.out.store.σ4@0"))
-        )
+        self.assertIsNotNone(g_sketch.lookup(parse_var("g.out.store.σ4@0")))
 
         self.assertEqual(
-            g_sketch.lookup(SchemaParser.parse_variable("g.out.store.σ4@0")),
-            g_sketch.lookup(SchemaParser.parse_variable("g.out")),
+            g_sketch.lookup(parse_var("g.out.store.σ4@0")),
+            g_sketch.lookup(parse_var("g.out")),
         )
         self.assertEqual(
-            g_sketch.lookup(SchemaParser.parse_variable("g.out.store.σ4@4")),
-            g_sketch.lookup(SchemaParser.parse_variable("g.out")),
+            g_sketch.lookup(parse_var("g.out.store.σ4@4")),
+            g_sketch.lookup(parse_var("g.out")),
         )
         self.assertEqual(
-            g_sketch.lookup(
-                SchemaParser.parse_variable("g.out.store.σ4@8")
-            ).lower_bound,
+            g_sketch.lookup(parse_var("g.out.store.σ4@8")).lower_bound,
             lattice._int,
         )
 
@@ -356,7 +334,7 @@ class RecursiveSchemaTest(unittest.TestCase):
         we weren't following the entire chain (transitively) to get the atomic
         type information (for the lattice).
         """
-        nf_apply = SchemaParser.parse_variable("nf")
+        nf_apply = parse_var("nf")
 
         constraints = {nf_apply: ConstraintSet()}
         constraint_str = """
@@ -376,7 +354,7 @@ class RecursiveSchemaTest(unittest.TestCase):
         for line in constraint_str.split("\n"):
             line = line.strip()
             if line.strip():
-                constraints[nf_apply].add(SchemaParser.parse_constraint(line))
+                constraints[nf_apply].add(parse_cs(line))
 
         program = Program(DummyLattice(), {}, constraints, {nf_apply: []})
         solver = Solver(program, verbose=True)
@@ -384,16 +362,12 @@ class RecursiveSchemaTest(unittest.TestCase):
 
         nf_sketches = sketches[nf_apply]
         self.assertEqual(
-            nf_sketches.lookup(
-                SchemaParser.parse_variable("nf.in_0.load.σ4@4")
-            ).upper_bound,
-            SchemaParser.parse_variable("int"),
+            nf_sketches.lookup(parse_var("nf.in_0.load.σ4@4")).upper_bound,
+            parse_var("int"),
         )
         self.assertEqual(
-            nf_sketches.lookup(
-                SchemaParser.parse_variable("nf.in_0.load.σ4@0")
-            ).upper_bound,
-            SchemaParser.parse_variable("int"),
+            nf_sketches.lookup(parse_var("nf.in_0.load.σ4@0")).upper_bound,
+            parse_var("int"),
         )
 
     def test_vListInsert_issue9(self):
@@ -502,26 +476,16 @@ class InferTypesTest(unittest.TestCase):
         (gen_cs, sketches) = compute_sketches(constraints, callgraph)
 
         f_sketch = sketches[DerivedTypeVariable("f")]
+        self.assertIsNotNone(f_sketch.lookup(parse_var("f.in_0")))
         self.assertIsNotNone(
-            f_sketch.lookup(SchemaParser.parse_variable("f.in_0"))
-        )
-        self.assertIsNotNone(
-            f_sketch.lookup(
-                SchemaParser.parse_variable("f.in_0.load.σ1@0.load.σ4@4")
-            )
+            f_sketch.lookup(parse_var("f.in_0.load.σ1@0.load.σ4@4"))
         )
 
         g_sketch = sketches[DerivedTypeVariable("g")]
+        self.assertIsNotNone(g_sketch.lookup(parse_var("g.out")))
+        self.assertIsNotNone(g_sketch.lookup(parse_var("g.out.load.σ1@0")))
         self.assertIsNotNone(
-            g_sketch.lookup(SchemaParser.parse_variable("g.out"))
-        )
-        self.assertIsNotNone(
-            g_sketch.lookup(SchemaParser.parse_variable("g.out.load.σ1@0"))
-        )
-        self.assertIsNotNone(
-            g_sketch.lookup(
-                SchemaParser.parse_variable("g.out.load.σ1@0.load.σ4@4")
-            )
+            g_sketch.lookup(parse_var("g.out.load.σ1@0.load.σ4@4"))
         )
 
     def test_input_arg_capability_transitive(self):
@@ -536,18 +500,12 @@ class InferTypesTest(unittest.TestCase):
         (gen_cs, sketches) = compute_sketches(constraints, callgraph)
 
         g_sketch = sketches[DerivedTypeVariable("g")]
+        self.assertIsNotNone(g_sketch.lookup(parse_var("g.in_0")))
         self.assertIsNotNone(
-            g_sketch.lookup(SchemaParser.parse_variable("g.in_0"))
+            g_sketch.lookup(parse_var("g.in_0.load.σ1@0.load.σ4@4"))
         )
         self.assertIsNotNone(
-            g_sketch.lookup(
-                SchemaParser.parse_variable("g.in_0.load.σ1@0.load.σ4@4")
-            )
-        )
-        self.assertIsNotNone(
-            g_sketch.lookup(
-                SchemaParser.parse_variable("g.out.load.σ1@0.load.σ4@4")
-            )
+            g_sketch.lookup(parse_var("g.out.load.σ1@0.load.σ4@4"))
         )
 
 
@@ -560,30 +518,26 @@ class CTypeTest(unittest.TestCase):
          F1 -
             |-> F3  (other part of struct fields)
         """
-        F1 = SchemaParser.parse_variable("F1")
-        F2 = SchemaParser.parse_variable("F2")
-        F3 = SchemaParser.parse_variable("F3")
+        F1 = parse_var("F1")
+        F2 = parse_var("F2")
+        F3 = parse_var("F3")
 
         constraints = {
             F1: ConstraintSet(),
             F2: ConstraintSet(),
             F3: ConstraintSet(),
         }
-        constraints[F1].add(SchemaParser.parse_constraint("F1.in_0 ⊑ A"))
-        constraints[F1].add(SchemaParser.parse_constraint("A ⊑ F2.in_1"))
-        constraints[F1].add(SchemaParser.parse_constraint("A ⊑ F3.in_2"))
+        constraints[F1].add(parse_cs("F1.in_0 ⊑ A"))
+        constraints[F1].add(parse_cs("A ⊑ F2.in_1"))
+        constraints[F1].add(parse_cs("A ⊑ F3.in_2"))
         # F2 accesses fields at offsets 0, 12
-        constraints[F2].add(SchemaParser.parse_constraint("F2.in_1 ⊑ B"))
-        constraints[F2].add(SchemaParser.parse_constraint("B.load.σ8@0 ⊑ int"))
-        constraints[F2].add(
-            SchemaParser.parse_constraint("B.load.σ4@12 ⊑ int")
-        )
+        constraints[F2].add(parse_cs("F2.in_1 ⊑ B"))
+        constraints[F2].add(parse_cs("B.load.σ8@0 ⊑ int"))
+        constraints[F2].add(parse_cs("B.load.σ4@12 ⊑ int"))
         # F3 accesses fields at offsets 8, 20
-        constraints[F3].add(SchemaParser.parse_constraint("F3.in_2 ⊑ C"))
-        constraints[F3].add(SchemaParser.parse_constraint("C.load.σ2@8 ⊑ int"))
-        constraints[F3].add(
-            SchemaParser.parse_constraint("C.load.σ8@20 ⊑ int")
-        )
+        constraints[F3].add(parse_cs("F3.in_2 ⊑ C"))
+        constraints[F3].add(parse_cs("C.load.σ2@8 ⊑ int"))
+        constraints[F3].add(parse_cs("C.load.σ8@20 ⊑ int"))
 
         lattice = DummyLattice()
         lattice_ctypes = DummyLatticeCTypes()
@@ -610,18 +564,14 @@ class CTypeTest(unittest.TestCase):
         Model that strcpy() is called with a field from a struct as the destination, which
         _should_ tell us that the given field is a string.
         """
-        F1 = SchemaParser.parse_variable("F1")
-        strcpy = SchemaParser.parse_variable("strcpy")
+        F1 = parse_var("F1")
+        strcpy = parse_var("strcpy")
 
         constraints = {F1: ConstraintSet(), strcpy: ConstraintSet()}
-        constraints[F1].add(SchemaParser.parse_constraint("F1.in_0 ⊑ A"))
-        constraints[F1].add(
-            SchemaParser.parse_constraint("A.load.σ8@8 ⊑ strcpy.in_1")
-        )
+        constraints[F1].add(parse_cs("F1.in_0 ⊑ A"))
+        constraints[F1].add(parse_cs("A.load.σ8@8 ⊑ strcpy.in_1"))
         constraints[strcpy].add(
-            SchemaParser.parse_constraint(
-                "strcpy.in_1.load.σ1@0*[nullterm] ⊑ int"
-            )
+            parse_cs("strcpy.in_1.load.σ1@0*[nullterm] ⊑ int")
         )
 
         lattice = DummyLattice()
@@ -646,17 +596,13 @@ class CTypeTest(unittest.TestCase):
         """
         Illustration of how a model of memcpy might work.
         """
-        F1 = SchemaParser.parse_variable("F1")
-        memcpy = SchemaParser.parse_variable("memcpy")
-        some_global = SchemaParser.parse_variable("some_global")
+        F1 = parse_var("F1")
+        memcpy = parse_var("memcpy")
+        some_global = parse_var("some_global")
 
         constraints = {F1: ConstraintSet()}
-        constraints[F1].add(
-            SchemaParser.parse_constraint("some_global ⊑ memcpy.in_1")
-        )
-        constraints[F1].add(
-            SchemaParser.parse_constraint("memcpy.in_1.load.σ4@0*[10] ⊑ int")
-        )
+        constraints[F1].add(parse_cs("some_global ⊑ memcpy.in_1"))
+        constraints[F1].add(parse_cs("memcpy.in_1.load.σ4@0*[10] ⊑ int"))
 
         lattice = DummyLattice()
         lattice_ctypes = DummyLatticeCTypes()
@@ -678,25 +624,17 @@ class CTypeTest(unittest.TestCase):
         Half of struct fields are only loaded and half are only stored, should still result
         in all fields being properly inferred.
         """
-        F1 = SchemaParser.parse_variable("F1")
-        some_global = SchemaParser.parse_variable("some_global")
+        F1 = parse_var("F1")
+        some_global = parse_var("some_global")
 
         constraints = {F1: ConstraintSet()}
-        constraints[F1].add(SchemaParser.parse_constraint("some_global ⊑ A"))
-        constraints[F1].add(SchemaParser.parse_constraint("A.load.σ4@0 ⊑ int"))
-        constraints[F1].add(
-            SchemaParser.parse_constraint("int ⊑ A.store.σ4@4")
-        )
-        constraints[F1].add(SchemaParser.parse_constraint("A.load.σ4@8 ⊑ int"))
-        constraints[F1].add(
-            SchemaParser.parse_constraint("int ⊑ A.store.σ4@12")
-        )
-        constraints[F1].add(
-            SchemaParser.parse_constraint("A.load.σ4@16 ⊑ int")
-        )
-        constraints[F1].add(
-            SchemaParser.parse_constraint("int ⊑ A.store.σ4@20")
-        )
+        constraints[F1].add(parse_cs("some_global ⊑ A"))
+        constraints[F1].add(parse_cs("A.load.σ4@0 ⊑ int"))
+        constraints[F1].add(parse_cs("int ⊑ A.store.σ4@4"))
+        constraints[F1].add(parse_cs("A.load.σ4@8 ⊑ int"))
+        constraints[F1].add(parse_cs("int ⊑ A.store.σ4@12"))
+        constraints[F1].add(parse_cs("A.load.σ4@16 ⊑ int"))
+        constraints[F1].add(parse_cs("int ⊑ A.store.σ4@20"))
 
         lattice = DummyLattice()
         lattice_ctypes = DummyLatticeCTypes()
@@ -716,14 +654,14 @@ class CTypeTest(unittest.TestCase):
 
     def test_tight_bounds_in(self):
         constraints = ConstraintSet()
-        constraints.add(SchemaParser.parse_constraint("f.in_0 ⊑ A"))
-        constraints.add(SchemaParser.parse_constraint("int ⊑ A"))
-        constraints.add(SchemaParser.parse_constraint("A ⊑ int"))
-        f = SchemaParser.parse_variable("f")
+        constraints.add(parse_cs("f.in_0 ⊑ A"))
+        constraints.add(parse_cs("int ⊑ A"))
+        constraints.add(parse_cs("A ⊑ int"))
+        f = parse_var("f")
         program = Program(DummyLattice(), set(), {f: constraints}, {f: {}})
         solver = Solver(program, verbose=LogLevel.DEBUG)
         (gen_const, sketches) = solver()
-        f_in1 = SchemaParser.parse_variable("f.in_0")
+        f_in1 = parse_var("f.in_0")
         self.assertEqual(sketches[f].lookup(f_in1).upper_bound, CLattice._int)
         gen = CTypeGenerator(
             sketches,
@@ -740,16 +678,16 @@ class CTypeTest(unittest.TestCase):
 
     def test_tight_bounds_out(self):
         constraints = ConstraintSet()
-        constraints.add(SchemaParser.parse_constraint("A ⊑ f.out"))
-        constraints.add(SchemaParser.parse_constraint("int ⊑ A"))
-        constraints.add(SchemaParser.parse_constraint("A ⊑ int"))
-        f = SchemaParser.parse_variable("f")
+        constraints.add(parse_cs("A ⊑ f.out"))
+        constraints.add(parse_cs("int ⊑ A"))
+        constraints.add(parse_cs("A ⊑ int"))
+        f = parse_var("f")
         program = Program(DummyLattice(), set(), {f: constraints}, {f: {}})
         solver = Solver(program, verbose=LogLevel.DEBUG)
         (gen_const, sketches) = solver()
         print(gen_const[f])
         print(sketches[f])
-        f_out = SchemaParser.parse_variable("f.out")
+        f_out = parse_var("f.out")
         self.assertEqual(sketches[f].lookup(f_out).lower_bound, CLattice._int)
         gen = CTypeGenerator(
             sketches,
